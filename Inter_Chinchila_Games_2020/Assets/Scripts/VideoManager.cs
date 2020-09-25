0aa40371c83c80a4fd9fb6156b767f6b52db3b3a
César Neves Mosca <Cronokami@hotmail.com>
2020-09-25 04:49:24 -0300

    Video no começo
    Adicionando vídeo placeholder no começo da cena. Fácil de substituir, pode testar jogando videos da pasta de videos no video player

---
+++ Inter_Chinchila_Games_2020/Assets/Scripts/VideoManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+	private VideoPlayer m_VideoPlayer;
+
+	void Awake()
+	{
+		m_VideoPlayer = GetComponent<VideoPlayer>();
+		m_VideoPlayer.loopPointReached += OnMovieFinished;
+	}
+
+	void OnMovieFinished(VideoPlayer player)
+	{
+		player.Stop();
+		Destroy(gameObject);
+		
+	}
+}

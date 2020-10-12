8a1b06967d3c61fda05140f35a6b712e8269422f
César Neves Mosca <Cronokami@hotmail.com>
2020-10-12 06:57:14 -0300

    Scroll para páginas da fase 2
    juro que tentei fazer possível mudar pra horizontal mas dps de um tempo começou a virar dor de cabeça dms pra algo que a gente nem falou sobre e nem faz sentido no momento, mas querendo adicionar páginas é só botar no ScrollManager, e desativar as que... não forem pra ficar ativas
    de boas, o código se vira com onde colocar as ativadas e as desativadas sem dar conflito(e só mexe as ativas)

---
+++ Assets/Scripts/Fase/2/FingerScroll.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerScroll : MonoBehaviour
+{
+	public GameObject[] targetObject;
+
+	public float deccelSpeed;
+
+	private float speed;
+	private float oldTouchPosition;
+	private bool manualMove;
+
+
+	public bool outOfBounds;
+	public Transform whereToGoBack;
+
+	private void Update()
+	{
+		for (int i = 0; i < targetObject.Length; i++)
+		{
+			if (!manualMove)
+			{
+				if (!outOfBounds)
+				{
+					speed = Mathf.Lerp(speed, 0, deccelSpeed);
+				}
+				else
+				{
+					speed = Vector3.Distance(targetObject[i].transform.position, whereToGoBack.position) * Mathf.Clamp(targetObject[i].transform.position.y - whereToGoBack.position.y, -5, 5);
+				}
+			}
+
+			if (targetObject[i].activeSelf)
+			{
+				targetObject[i].transform.localPosition -= new Vector3(0, speed, 0);
+			}
+			else
+			{
+				targetObject[i].transform.localPosition = new Vector3(0, 0, 0);
+			}
+		}
+	}
+
+	private void OnMouseDown()
+	{
+		oldTouchPosition = Input.mousePosition.y;
+		manualMove = true;
+	}
+	private void OnMouseDrag()
+	{
+		speed = oldTouchPosition - Input.mousePosition.y;
+		oldTouchPosition -= speed;
+	}
+	private void OnMouseUp()
+	{
+		manualMove = false;
+	}
+
+}
---
+++ Assets/Scripts/Fase/2/KeepInFrame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepInFrame : MonoBehaviour
+{
+	public FingerScroll fingerScroll;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		fingerScroll.outOfBounds = false;
+	}
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		Debug.Log("foi");
+		fingerScroll.outOfBounds = true;
+		fingerScroll.whereToGoBack = transform;
+	}
+}

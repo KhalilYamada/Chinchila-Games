8659da3a381d73d0bc810c3d471db8a4d49171f9
César Neves Mosca <Cronokami@hotmail.com>
2020-10-28 02:54:00 -0300

    Iniciando keyboard na fase 2

---
+++ Assets/Scripts/Fase/2/NotebookTextWriter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NotebookTextWriter : MonoBehaviour
+{
+	public TMP_Text text;
+
+
+	public void AddChar(string character)
+	{
+		text.text += character;
+	}
+
+}

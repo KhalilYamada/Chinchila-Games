21e0fe098ab3e180b4428afa5c0139965e14cc39
Brian Luis <brian.souto@gmail.com>
2020-11-06 22:48:45 -0300

    Ajustes
    Novo script para trocar de cenas/sair do jogo e alguns ajustes de ancora

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Cena1()
+    {
+        SceneManager.LoadScene("Fase 1 Prot");
+    }
+
+    public void Cena2()
+    {
+        SceneManager.LoadScene("Fase 2 Prot");
+    }
+
+    public void Cena3()
+    {
+        SceneManager.LoadScene("Fase 3 Prot");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Saiu");
+        Application.Quit();
+    }
+}
+

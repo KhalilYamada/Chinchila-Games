0b2c36aef69d8b78ce9d1f90d0af92714ea06d3d
Brian Luis <brian.souto@gmail.com>
2020-11-17 15:00:24 -0300

    Sliders no menuConfig funcionando

---
+++ Assets/Scripts/FmodSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FmodSound : MonoBehaviour
+{
+    FMOD.Studio.Bus Musicas;
+    FMOD.Studio.Bus Efeitos;
+    float MusicVolume;
+    float EfeitosVolume;
+    public GameObject[] sliders;
+
+    void Awake()
+    {
+        Musicas = FMODUnity.RuntimeManager.GetBus("bus:/Mestre/Musicas");
+        Efeitos = FMODUnity.RuntimeManager.GetBus("bus:/Mestre/Efeitos");
+        sliders[0].GetComponent<Slider>().value = PlayerPrefs.GetFloat("volumeMusica");
+        sliders[1].GetComponent<Slider>().value = PlayerPrefs.GetFloat("volumeEfeitos");
+        //MusicVolume = PlayerPrefs.GetFloat("volumeMusica");
+        //EfeitosVolume = PlayerPrefs.GetFloat("volumeEfeitos");
+    }
+
+    void Update()
+    {
+        Musicas.setVolume(PlayerPrefs.GetFloat("volumeMusica"));
+        Efeitos.setVolume(PlayerPrefs.GetFloat("volumeEfeitos"));
+    }
+
+    public void MusicVolumeLevel(float newMusicVolume)
+    {
+        MusicVolume = newMusicVolume;
+        PlayerPrefs.SetFloat("volumeMusica", newMusicVolume);
+    }
+
+    public void EfeitosVolumeLevel(float newEfeitosVolume)
+    {
+        EfeitosVolume = newEfeitosVolume;
+        PlayerPrefs.SetFloat("volumeEfeitos", newEfeitosVolume);
+
+    }
+}

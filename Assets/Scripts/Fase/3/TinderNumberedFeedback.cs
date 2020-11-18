00086e46a470551bdaddd17b3e8ae7e31ce2b950
César Neves Mosca <Cronokami@hotmail.com>
2020-11-18 17:52:46 -0300

    Feedback dos números na fase 3

---
+++ Assets/Scripts/Fase/3/TinderNumberedFeedback.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TinderNumberedFeedback : MonoBehaviour
+{
+	public TinderManager tinderManager;
+
+	public Sprite[] sprites;
+	private Image thisImage;
+	private Sprite[] usefulSprites;
+
+
+	private void Start()
+	{
+		thisImage = GetComponent<Image>();
+
+		usefulSprites = new Sprite[tinderManager.unitInfo.Length];
+		int index = 0;
+		for (int i = 0; i < tinderManager.unitInfo.Length; i++)
+		{
+			index += i + 1;
+		}
+		index--;
+
+		for (int i = 0; i < usefulSprites.Length; i++)
+		{
+			usefulSprites[i] = sprites[index + i];
+		}
+
+		thisImage.sprite = usefulSprites[TinderManager.index];
+	}
+
+	void Update()
+	{
+		thisImage.sprite = usefulSprites[TinderManager.index];
+	}
+}

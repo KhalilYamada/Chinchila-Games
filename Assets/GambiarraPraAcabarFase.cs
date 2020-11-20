9eeea0fa1b2f1bc98cc0e4ce58a6f88604b6ae05
César Neves Mosca <Cronokami@hotmail.com>
2020-11-19 21:45:58 -0300

    Gambiarra final pra fase 2

---
+++ Assets/GambiarraPraAcabarFase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GambiarraPraAcabarFase : MonoBehaviour
+{
+	private void OnEnable()
+	{
+		PlayerPrefs.SetInt("Finished " + SceneManager.GetActiveScene().name, 1);
+		SceneManager.LoadScene("Menu");
+	}
+}
